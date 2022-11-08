dbfd8d50f35d78b54094517b7fe56f2e9179dfdc
Artur-Niezgoda <110778550+Artur-Niezgoda@users.noreply.github.com>
2022-11-08 01:06:02 +0100

    add authentication, create playlist, search and add songs

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from environs import Env
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 
-def check_valid_date(splited_date):
-    year = splited_date[0]
-    month = splited_date[1]
-    day = splited_date[2]
+def check_valid_date(split_date):
+    year = split_date[0]
+    month = split_date[1]
+    day = split_date[2]
     try:
         year = int(year)
         month = int(month)
@@ -14,12 +17,12 @@ def check_valid_date(splited_date):
         if year < 1958 or year > datetime.now().year:
             print("Invalid year value")
             return False
-        if month > 12  or month < 1:
+        if month > 12 or month < 1:
             print("Invalid month value")
             return False
         if (month in [1, 3, 5, 7, 8, 10, 12] and (day > 31 or day < 1)) or \
-            (month in [4, 6, 9,11] and (day > 30 or day < 1)) or \
-            (month == 2 and (day> 28 or day < 1)):
+                (month in [4, 6, 9, 11] and (day > 30 or day < 1)) or \
+                (month == 2 and (day > 28 or day < 1)):
             print("Invalid day value for a given month")
             return False
         return True
@@ -28,14 +31,61 @@ def check_valid_date(splited_date):
         return False
 
 
+# Read environment variables from env file
+env = Env()
+env.read_env()
+spotipy_client_id = env("SPOTIPY_CLIENT_ID")
+spotipy_client_secret = env("SPOTIPY_CLIENT_SECRET")
+redirect_uri = env("SPOTIPY_REDIRECT_URI")
+
+date = "1958-01-01"
 is_date_correct = False
 while not is_date_correct:
     date = input("What year would you like to travel to (up to year 1958)? Please type the date in this "
                  "format YYYY-MM-DD:")
-    split_date = date.split("-")
-    is_date_correct = check_valid_date(split_date)
+    is_date_correct = check_valid_date(date.split("-"))
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
-
+# Get the Website
+URL = f"https://www.billboard.com/charts/hot-100/{date}"
 response = requests.get(URL)
 response.encoding = "utf-8"
+
+# Scrape
+soup = BeautifulSoup(response.text, 'html.parser')
+
+songs = soup.find_all("h3", class_="a-no-trucate")
+artists = soup.find_all("span", class_="a-no-trucate")
+
+# Initialize authentication
+spotify = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(client_id=spotipy_client_id,
+                              client_secret=spotipy_client_secret,
+                              scope="playlist-modify-private",
+                              redirect_uri=redirect_uri,
+                              show_dialog=True,
+                              cache_path="token.txt"))
+
+user_id = spotify.current_user()["id"]
+
+# Create a playlist
+playlist = spotify.user_playlist_create(user=user_id,
+                                        name=f"Pythonista playlist ({date})",
+                                        public=False,
+                                        description="playlist created by python code")
+
+# Initialize query
+item_list = []
+for index in range(len(artists)):
+    song = songs[index].getText()
+    artist = artists[index].getText()
+
+    query = spotify.search(q=f"track:{song} artist:{artist}", type="track", limit=1, market="US")
+    try:
+        song_uri = query["tracks"]["items"][0]["uri"]
+    except IndexError:
+        print(f"track not found")
+        continue
+    else:
+        item_list.append(song_uri)
+
+spotify.playlist_add_items(playlist_id=playlist["id"], items=item_list) # add all found songs to the playlist
